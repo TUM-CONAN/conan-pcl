f9a5e4a3032677fa64cd29ed83448240cb745a72
greenjava <greenjava@gmail.com>
2018-10-09 23:53:07 +0200

    chore(gitlab-ci): update conan matrix build configs

--- build.py
+++ build.py
@@ -6,14 +6,11 @@ if __name__ == "__main__":
         username="sight", 
         visual_runtimes=["MD", "MDd"],
         archs=["x86_64"])
-    if os.getenv("CONAN_USE_CUDA", "False") == "True":
-        builder.add({"arch": "x86_64", "build_type": "Debug"},
-                    {"pcl:shared": True, "pcl:use_cuda": True})
-        builder.add({"arch": "x86_64", "build_type": "Release"},
-                    {"pcl:shared": True, "pcl:use_cuda": True})
-    else:
-        builder.add({"arch": "x86_64", "build_type": "Debug"},
-                    {"pcl:shared": True, "pcl:use_cuda": False})
-        builder.add({"arch": "x86_64", "build_type": "Release"},
-                    {"pcl:shared": True, "pcl:use_cuda": False})
+    builder.add_common_builds(shared_option_name=False, pure_c=True)
+    filtered_builds = []
+    use_cuda = (os.getenv("CONAN_USE_CUDA", "False") == "True")
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        options["pcl:use_cuda"] = use_cuda
+        filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
