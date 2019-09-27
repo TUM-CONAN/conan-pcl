ff7d03c0bbeea4f476124760c04a3e699a4f3fd1
Didier WECKMANN <didier.weckmann@ircad.fr>
2019-09-27 21:34:34 +0200

    feat(conanfile): use stable

--- conanfile.py
+++ conanfile.py
@@ -47,16 +47,16 @@ class LibPCLConan(ConanFile):
             os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
 
     def requirements(self):
-        self.requires("common/1.0.1@sight/testing")
-        self.requires("qt/5.12.4-r1@sight/testing")
-        self.requires("eigen/3.3.7-r2@sight/testing")
-        self.requires("boost/1.69.0-r3@sight/testing")
-        self.requires("vtk/8.2.0-r3@sight/testing")
-        self.requires("openni/2.2.0-r4@sight/testing")
-        self.requires("flann/1.9.1-r4@sight/testing")
+        self.requires("common/1.0.1@sight/stable")
+        self.requires("qt/5.12.4-r1@sight/stable")
+        self.requires("eigen/3.3.7-r2@sight/stable")
+        self.requires("boost/1.69.0-r3@sight/stable")
+        self.requires("vtk/8.2.0-r3@sight/stable")
+        self.requires("openni/2.2.0-r4@sight/stable")
+        self.requires("flann/1.9.1-r4@sight/stable")
 
         if tools.os_info.is_windows:
-            self.requires("zlib/1.2.11-r3@sight/testing")
+            self.requires("zlib/1.2.11-r3@sight/stable")
 
     def build_requirements(self):
         if tools.os_info.linux_distro == "linuxmint":
@@ -94,7 +94,6 @@ class LibPCLConan(ConanFile):
         )
 
         cmake = CMake(self)
-        cmake.verbose = True
 
         cmake.definitions["BUILD_apps"] = "OFF"
         cmake.definitions["BUILD_examples"] = "OFF"
