558204b34514fdfb56df758e70725b4abd9ab327
SinisterMJ <Anton_Roth@gmx.de>
2021-03-01 13:28:36 +0100

    Working PCL 1.10.1

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,12 @@
 import os
 
-from fnmatch import fnmatch
 from conans import ConanFile, CMake, tools
 
 
 class LibPCLConan(ConanFile):
     name = "pcl"
     upstream_version = "1.10.1"
-    package_revision = "-r1"
+    package_revision = ""
     version = "{0}{1}".format(upstream_version, package_revision)
 
     generators = "cmake"
@@ -16,22 +15,15 @@ class LibPCLConan(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False],
         "with_cuda": [True, False],
+        "with_openni": [True, False],
     }
     default_options = [
         "shared=True",
         "fPIC=True",
-        "with_cuda=True"
+        "with_cuda=True",
+        "with_openni=False",
     ]
     default_options = tuple(default_options)
-    exports = [
-        # "patches/clang_macos.diff",
-        # "patches/kinfu.diff",
-        # "patches/pcl_eigen.diff",
-        # "patches/pcl_gpu_error.diff",
-        # "patches/point_cloud.diff",
-        # "patches/pcl_supervoxel_clustering.diff",
-        # "patches/cmake_add_new_boost_versions.diff",
-    ]
     url = "https://git.ircad.fr/conan/conan-pcl"
     license = "BSD License"
     description = "The Point Cloud Library is for 2D/3D image and point cloud processing."
@@ -43,39 +35,26 @@ class LibPCLConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        # PCL is not well prepared for c++ standard > 11...
-        del self.settings.compiler.cppstd
 
-        # if 'CI' not in os.environ:
-        #     os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
-
-        if self.settings.os == "Linux":
-            self.options["Boost"].fPIC = True
-
-        if tools.os_info.is_windows:
-            self.options["Boost"].shared=True
-
+        if 'CI' not in os.environ:
+            os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
 
     def requirements(self):
         self.requires("ircad_common/1.0.2@camposs/stable")
         self.requires("qt/5.12.4-r2@camposs/stable")
-        self.requires("eigen/3.3.7@camposs/stable")
-        self.requires("Boost/1.72.0@camposs/stable")
+        self.requires("eigen/3.3.9@camposs/stable")
+        self.requires("Boost/1.72.0@camposs/stable",)
         self.requires("vtk/8.2.0-r4@camposs/stable")
-        self.requires("openni/2.2.0-r3@camposs/stable")
-        self.requires("flann/1.9.1-r2@camposs/stable")
+        self.requires("flann/1.9.1-r5@camposs/stable")
 
+        if self.options.with_openni:
+            self.requires("openni/2.2.0-r3@camposs/stable")
+        
         if tools.os_info.is_windows:
             self.requires("zlib/1.2.11@camposs/stable")
 
-        if self.options.with_cuda:
-            self.requires("cuda_dev_config/[>=1.0]@camposs/stable")
-
     def build_requirements(self):
         if tools.os_info.linux_distro == "linuxmint":
-            installer = tools.SystemPackageTool()
-            installer.install("zlib1g-dev")
-        if tools.os_info.linux_distro == "ubuntu":
             installer = tools.SystemPackageTool()
             installer.install("zlib1g-dev")
 
@@ -83,77 +62,17 @@ class LibPCLConan(ConanFile):
         if tools.os_info.linux_distro == "linuxmint":
             installer = tools.SystemPackageTool()
             installer.install("zlib1g")
-        if tools.os_info.linux_distro == "ubuntu":
-            installer = tools.SystemPackageTool()
-            installer.install("zlib1g")
 
     def source(self):
+        # Use our fork, until our MR is merged. see https://github.com/PointCloudLibrary/pcl/pull/3741
         tools.get(
-            "https://github.com/PointCloudLibrary/pcl/archive/pcl-{0}.tar.gz".format(
+            "https://github.com/IRCAD-IHU/pcl/archive/pcl-{0}-sight.tar.gz".format(
                 self.upstream_version))
         os.rename(
-            "pcl-pcl-{0}".format(self.upstream_version),
+            "pcl-pcl-{0}-sight".format(self.upstream_version),
             self.source_subfolder)
 
     def build(self):
-        pcl_source_dir = os.path.join(
-            self.source_folder, self.source_subfolder)
-        # tools.patch(pcl_source_dir, "patches/clang_macos.diff")
-        # tools.patch(pcl_source_dir, "patches/kinfu.diff")
-        # tools.patch(pcl_source_dir, "patches/pcl_eigen.diff")
-        # tools.patch(pcl_source_dir, "patches/pcl_gpu_error.diff")
-        # tools.patch(pcl_source_dir, "patches/point_cloud.diff")
-        # tools.patch(pcl_source_dir, "patches/pcl_supervoxel_clustering.diff")
-        # tools.patch(pcl_source_dir, "patches/cmake_add_new_boost_versions.diff")
-
-        # patch for cuda arch >7.0
-
-        # for path, subdirs, names in os.walk(pcl_source_dir,):
-        #     for name in names:
-        #         if fnmatch(name, "*.cu"):
-        #             wildcard_file = os.path.join(path, name)
-
-        #             # Fix package_folder paths
-        #             tools.replace_in_file(
-        #                 wildcard_file, "__all(", "__all_sync(0xFFFFFFFF,", strict=False)
-        #             tools.replace_in_file(
-        #                 wildcard_file, "__any(", "__any_sync(0xFFFFFFFF,", strict=False)
-        #             tools.replace_in_file(
-        #                 wildcard_file, "__ballot(",
-        #                 "__ballot_sync(0xFFFFFFFF,", strict=False)
-
-        # Use our own FindFLANN which take care of conan..
-        # os.remove(
-        #     os.path.join(
-        #         pcl_source_dir,
-        #         'cmake',
-        #         'Modules',
-        #         'FindFLANN.cmake'))
-
-        tools.replace_in_file(os.path.join(self.source_subfolder, "common", "include", "pcl", "pcl_macros.h"),
-            """
-  using uint8_t PCL_DEPRECATED("use std::uint8_t instead of pcl::uint8_t") = std::uint8_t;
-  using int8_t PCL_DEPRECATED("use std::int8_t instead of pcl::int8_t") = std::int8_t;
-  using uint16_t PCL_DEPRECATED("use std::uint16_t instead of pcl::uint16_t") = std::uint16_t;
-  using int16_t PCL_DEPRECATED("use std::uint16_t instead of pcl::int16_t") = std::int16_t;
-  using uint32_t PCL_DEPRECATED("use std::uint32_t instead of pcl::uint32_t") = std::uint32_t;
-  using int32_t PCL_DEPRECATED("use std::int32_t instead of pcl::int32_t") = std::int32_t;
-  using uint64_t PCL_DEPRECATED("use std::uint64_t instead of pcl::uint64_t") = std::uint64_t;
-  using int64_t PCL_DEPRECATED("use std::int64_t instead of pcl::int64_t") = std::int64_t;
-  using int_fast16_t PCL_DEPRECATED("use std::int_fast16_t instead of pcl::int_fast16_t") = std::int_fast16_t;
-""",
-            """
-  using uint8_t = std::uint8_t;
-  using int8_t = std::int8_t;
-  using uint16_t = std::uint16_t;
-  using int16_t = std::int16_t;
-  using uint32_t = std::uint32_t;
-  using int32_t = std::int32_t;
-  using uint64_t = std::uint64_t;
-  using int64_t = std::int64_t;
-  using int_fast16_t = std::int_fast16_t;
-""")
-
         # Import common flags and defines
         import common
 
@@ -198,7 +117,6 @@ class LibPCLConan(ConanFile):
         if self.options.with_cuda:
             cmake.definitions["BUILD_CUDA"] = "ON"
             cmake.definitions["BUILD_GPU"] = "ON"
-            cmake.definitions["BUILD_gpu_containers"] = "ON"
             cmake.definitions["BUILD_gpu_kinfu"] = "ON"
             cmake.definitions["BUILD_gpu_kinfu_large_scale"] = "ON"
             cmake.definitions["BUILD_visualization"] = "ON"
